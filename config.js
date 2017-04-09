be74167a2e82312a1fd15dcdcd305f43eea14171
tdonovan4 <tdonovan@codengineer.com>
2017-04-08 21:12:32 -0400

    Change config file
    - Change config file to JavaScript
    
    - Add comments for easier configuration
    
    - Add a language field for upcoming commit

---
+++ config.js
@@ -0,0 +1,12 @@
+//Configuration file
+//Please fill the fields
+//Your server ID
+const serverID = '';
+//The ID of the bot
+const botID = '';
+//Role name for "botMember"
+const roleMember = '';
+//Role name for "botModp"
+const roleModo = '';
+//Localization ('english' or 'french')
+const language = ''
