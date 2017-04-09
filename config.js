0cedfa5bf87c6c538035e28f3b938a7495060432
tdonovan4 <tdonovan@codengineer.com>
2017-04-08 21:22:33 -0400

    Change config

--- config.js
+++ config.js
@@ -1,12 +1,14 @@
 //Configuration file
 //Please fill the fields
-//Your server ID
-const serverID = '';
-//The ID of the bot
-const botID = '';
-//Role name for "botMember"
-const roleMember = '';
-//Role name for "botModp"
-const roleModo = '';
-//Localization ('english' or 'french')
-const language = ''
+module.exports = {
+	//Your server ID
+	serverID: '',
+	//The ID of the bot
+	botID: '',
+	//Role name for "botMember"
+	roleMember: '',
+	//Role name for "botModp"
+	roleModo: '',
+	//Localization ('english' or 'french')
+	language: ''
+}
