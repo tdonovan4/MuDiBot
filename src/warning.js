e565716224c60ccfd3080c7e7d9d3e0ed2d29a7c
tdonovan4 <tdonovan@codengineer.com>
2017-04-14 17:56:42 -0400

    Big cleaning
    - Create an object for the "$warn" command
    
    - Clean bot.js
    
    - Remove unused functions
    
    - Add warning-list.json to .gitignore

---
+++ src/warning.js
@@ -0,0 +1,76 @@
+const storage = require('./storage.js');
+module.exports = {
+	warningList: null,
+	warn: function (msg) {
+		let args = msg.content.split(" ").slice(1);
+		let users = msg.mentions.users.array();
+		var warningList;
+
+		storage.exist();
+		if (storage.empty()) {
+			warningList = {}
+			storage.write(warningList);
+		} else {
+			warningList = storage.read();
+		}
+
+		switch (args[0]) {
+		case null:
+			console.log('Not enough arguments');
+			break;
+		case 'clear':
+			if (args[1] == null) {
+				console.log('Not enough arguments');
+			} else if (args[1] === 'all') {
+				storage.delete ();
+				console.log('Storage cleared');
+			} else {
+				if (args[1].includes(users[0].id)) {
+					console.log(args[1]);
+					warningList[args[1]] = undefined;
+					storage.write(warningList);
+					console.log('User cleared');
+				}
+			}
+			break;
+		case 'list':
+			var string = '';
+			if (args[1] != null) {
+				if (args[1].includes(users[0].id)) {
+					string += args[1] + ': ' + warningList[args[1]] + ' warnings\n';
+				}
+			} else {
+				for (i = 0; i < Object.keys(warningList).length; i++) {
+					var user = Object.keys(warningList)[i]
+						string += user + ': ' + warningList[user] + ' warnings\n';
+				}
+			}
+			msg.channel.send(string);
+			break;
+		case 'remove':
+			if (args[1].includes(users[0].id)) {
+				if (args[1]in warningList) {
+					warningList[args[1]] -= 1;
+					msg.channel.send(args[1] + ': ' + warningList[args[1]] + ' warnings');
+					storage.write(warningList);
+					if (warningList[args[1]] <= 0) {
+						warningList[args[1]] = undefined;
+						storage.write(warningList);
+						console.log('User cleared');
+					}
+				}
+			}
+			break;
+		default:
+			if (args[0].includes(users[0].id)) {
+				if (args[0]in warningList) {
+					warningList[args[0]] += 1;
+				} else {
+					warningList[args[0]] = 1;
+				}
+				storage.write(warningList);
+				msg.channel.send(args[0] + ': ' + warningList[args[0]] + ' warnings');
+			}
+		}
+	}
+}
