cc73d15c478e41c28f76d3c55d0836a2225d7dfa
tdonovan4 <tdonovan@codengineer.com>
2017-04-18 18:57:21 -0400

    Create a new file for the audio player

---
+++ src/audio-player.js
@@ -0,0 +1,41 @@
+module.exports = {
+	currentVoice: null,
+	play: function(i, message) {
+		var channel = message.member.voiceChannel;
+		if (typeof channel !== "undefined") {
+			if (i === 0 && channel.connection != null) {
+				channel.connection.disconnect();
+			}
+			if (i === 1) {
+				channel.join()
+				.then(connection => {
+					dispatcher = connection.playFile('./sound/sound.mp3');
+					dispatcher.on('end', () => connection.disconnect());
+				})
+				.catch (console.error);
+			}
+			if (i === 2) {
+				channel.join()
+				.then(connection => {
+					dispatcher = connection.playFile('./sound/hello.wav');
+					dispatcher.on('end', () => connection.disconnect());
+				})
+				.catch (console.error);
+			}
+			if (i === 3) {
+				channel.join()
+				.then(connection => {
+					dispatcher = connection.playFile('./sound/explosion.wav');
+					dispatcher.on('end', () => {
+						connection.disconnect();
+						message.reply('Boom!');
+					});
+				})
+				.catch (console.error);
+			}
+			currentVoice = channel;
+		} else if (i === 3) {
+			message.reply('Boom!');
+		}
+	}
+}
