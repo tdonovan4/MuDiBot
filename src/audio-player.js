56005bc073e38d2524eea1a23e187cc51c51d141
tdonovan4 <tdonovan@codengineer.com>
2017-04-20 22:32:41 -0400

    Add "$play"
    - Add "$play" to play videos from Youtube (sound only), only one video
    can be played at the moment

--- src/audio-player.js
+++ src/audio-player.js
@@ -1,3 +1,4 @@
+const ytdl = require('ytdl-core');
 module.exports = {
 	currentVoice: null,
 	play: function (i, message) {
@@ -38,6 +39,17 @@ module.exports = {
 			currentVoice = channel;
 		} else if (i === 3) {
 			message.reply('Boom! ' + emoji);
+		}
+	},
+	playStream: function (message, link) {
+		var channel = message.member.voiceChannel;
+		if (typeof channel !== "undefined") {
+			channel.join()
+			.then(connection => {
+				const stream = ytdl('https://www.youtube.com/watch?v=dQw4w9WgXcQ', {filter : 'audioonly'});
+				dispatcher = connection.playStream(stream);
+				dispatcher.on('end', () => connection.disconnect());
+			})
 		}
 	},
 	stop: function (message) {
