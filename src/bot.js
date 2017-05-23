12082f867e3fb766d2b9172058641bfb4fd39959
tdonovan4 <tdonovan@codengineer.com>
2017-05-22 22:21:58 -0400

    Add comments

--- src/bot.js
+++ src/bot.js
@@ -1,4 +1,4 @@
-//TODO: Put more comments
+//Main class
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require('../config.js');
@@ -7,82 +7,105 @@ const warning = require('./warning.js');
 const player = require('./audio-player.js');
 var localization;
 
+//Log to the discord user  with the ID
 client.login(config.botID);
 
+//Start the bot
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.username}!`);
+
 	if (config.language === 'french') {
 		localization = data.french
 			console.log('french');
 	} else {
+		//Use english by default in case the chosen language is not found
 		localization = data.english
 			console.log('english');
 	}
+
 	client.user.setGame(localization.status);
 });
 
+/*
+ *Use an object containing command objects to get
+ *the permission needed and execute the command
+ */
 var commands = {
 	ping: {
+		//Reply "Pong!"
 		permLvl: "everyone",
 		execute: function (msg) {
 			msg.reply(localization.replies.ping);
 		}
 	},
 	gif: {
+		//A GIF of a robot, just a funny little feature
 		permLvl: "roleMember",
 		execute: function (msg) {
 			msg.reply('http://giphy.com/gifs/l4FGBpKfVMG4qraJG');
 		}
 	},
 	clearlog: {
+		//Clear listed commands
 		permLvl: "roleModo",
 		execute: function (msg) {
+			//Split the message to get only the argument
 			let args = msg.content.split(" ").slice(1);
-			let num = args[0];
+			let numToDel = args[0];
 
-			if (num === null || isNaN(num)) {
-				num = '50';
+			//In case the argument isn't a valid number
+			if (numToDel === null || isNaN(numToDel)) {
+				numToDel = '50';
 			}
 			clear(msg, num);
 		}
 	},
+	//TODO: delete this command
 	join: {
+		//Plays a song
 		permLvl: "roleMember",
 		execute: function (msg) {
 			player.play(1, msg);
 		}
 	},
 	hello: {
+		//Play a greeting sound and reply hi
 		permLvl: "everyone",
 		execute: function (msg) {
 			player.play(2, msg);
-			msg.reply(localization.replies.hello)
+			msg.reply(localization.replies.hello);
 		}
 	},
 	quit: {
+		//Stop the voice connection and leave voice channel
 		permLvl: "everyone",
 		execute: function (msg) {
 			player.stop(msg);
 		}
 	},
 	help: {
+		//Display a list of commands and their usage
 		permLvl: "everyone",
 		execute: function (msg) {
 			var help = localization.help;
 			var roles = msg.channel.guild.roles;
-			var helpString = '~Help~' + '\n'
-				for (i = 0; i < help.length; i++) {
-					for (n = 0; n < help[i].commands.length; n++) {
-						helpString += help[i].commands[n].name + help[i].commands[n].args + ' : [' +
-						mention(roles, commands[keys[i]].permLvl) + '] ' + help[i].commands[n].msg + '\n'
-					}
+
+			//Use a string to add all commands one by one
+			var helpString = '~Help~' + '\n';
+			for (i = 0; i < help.length; i++) {
+				for (n = 0; n < help[i].commands.length; n++) {
+					helpString += help[i].commands[n].name + help[i].commands[n].args + ' : [' +
+					mention(roles, commands[keys[i]].permLvl) + '] ' + help[i].commands[n].msg + '\n'
 				}
-				msg.channel.send(helpString);
+			}
+			msg.channel.send(helpString);
 		}
 	},
 	restart: {
+		//Restart the client
 		permLvl: "roleModo",
 		execute: function () {
+			//Spawn new process
 			var spawn = require('child_process').spawn;
 
 			var child = spawn('node', ['./src/bot.js'], {
@@ -90,15 +113,17 @@ var commands = {
 					shell: true,
 					stdio: 'ignore'
 				});
-			
 			child.unref();
+
 			console.log('Restarting');
 
+			//Exit this process
 			process.exitCode = 0;
 			process.exit();
 		}
 	},
 	info: {
+		//Display info about the client
 		permLvl: "everyone",
 		execute: function (msg) {
 			var pjson = require('../package.json');
@@ -114,6 +139,7 @@ var commands = {
 		}
 	},
 	tnt: {
+		//Play a big boom!
 		permLvl: "everyone",
 		execute: function (msg) {
 			msg.reply(localization.replies.tnt);
@@ -121,6 +147,7 @@ var commands = {
 		}
 	},
 	kill: {
+		//Kill the process
 		permLvl: "roleModo",
 		execute: function (msg) {
 			console.log('Restarting');
@@ -129,12 +156,14 @@ var commands = {
 		}
 	},
 	warn: {
+		//Handle warnings
 		permLvl: "roleModo",
 		execute: function (msg) {
 			warning.warn(msg);
 		}
 	},
 	play: {
+		//Play a song on YouTube
 		permLvl: "everyone",
 		execute: function (msg) {
 			player.playYoutube(msg, msg.content.split(" ").slice(1), config.youtubeAPIKey);
@@ -144,6 +173,10 @@ var commands = {
 
 var keys = Object.keys(commands);
 
+/*
+ *Function fired when a message is posted
+ *to check if the message is calling a command
+ */
 client.on('message', msg => {
 	if (msg.author.id != "290581674343792651") {
 		for (i = 0; i < keys.length; i++) {
@@ -157,6 +190,7 @@ client.on('message', msg => {
 	}
 });
 
+//Format time
 function time() {
 	var time = process.uptime();
 	var days = ~~(time / 86400)
@@ -166,20 +200,52 @@ function time() {
 	return days + 'd:' + hrs + 'h:' + mins + 'm:' + secs + 's'
 }
 
+//List of commands to clear (excluding all this bot commands)
 var commandsToClear = config.commandsToClear;
 var usersToClear = config.usersToClear;
 
+//Function that fetch, check and delete messages
 function clear(msg, num) {
+	//Fetch
 	msg.channel.fetchMessages({
 		limit: parseInt(num)
 	})
 	.then(messages => {
 		console.log(num)
+		//Check messages
 		for (var i = 0; i < messages.array().length; i++) {
+			//Find bot messages
 			if (messages.array()[i].author.id === client.user.id) {
 				messages.array()[i].delete ()
 			} else {
-				clearLoops(messages.array()[i]);
+				/*
+				 *TODO: Optimize loops by using one list 
+				 *	   with all needed to be deleted
+				 */
+				//Find bot commands
+				for (var n = 0; n < keys.length; n++) {
+					//We add a +1 because keys don't include the $
+					if (messages.content.substring(0, keys[n].length + 1) == '$' + keys[n]) {
+						messages.delete ()
+						break
+					}
+				}
+				/*
+				 *The two next loops check for commands and 
+				 *users to delete using the config file
+				 */
+				for (var n = 0; n < commandsToClear.length; n++) {
+					if (messages.content.includes(commandsToClear[n])) {
+						messages.delete ()
+						break;
+					}
+				}
+				for (var n = 0; n < usersToClear.length; n++) {
+					if (messages.author.id === usersToClear[n]) {
+						messages.delete ()
+						break;
+					}
+				}
 			}
 		}
 		console.log('Messages cleared!');
@@ -187,28 +253,7 @@ function clear(msg, num) {
 	.catch (console.error);
 }
 
-function clearLoops(messages) {
-	for (var n = 0; n < keys.length; n++) {
-		//We add a +1 because keys don't include the $
-		if (messages.content.substring(0, keys[n].length + 1) == '$' + keys[n]) {
-			messages.delete ()
-			return;
-		}
-	}
-	for (var n = 0; n < commandsToClear.length; n++) {
-		if (messages.content.includes(commandsToClear[n])) {
-			messages.delete ()
-			return;
-		}
-	}
-	for (var n = 0; n < usersToClear.length; n++) {
-		if (messages.author.id === usersToClear[n]) {
-			messages.delete ()
-			return;
-		}
-	}
-}
-
+//Convert roles into mention objects
 function mention(roles, role) {
 	if (role === 'everyone') {
 		return '@everyone';
@@ -220,22 +265,34 @@ function mention(roles, role) {
 		return null;
 	}
 }
+/*
+ *Check if the message author has permission 
+ *to do the command, return true or false
+ */
 function checkRole(msg, role) {
 	var permLevel = 0;
 	var currentPermLevel = 0;
-
+	
+	//TODO: add a list of exceptions in config
+	//Debug only, check if user is tdonovan4
 	if (msg.author.id === '265280961409843202') {
 		return true;
 	}
+	
+	//Check if user is an administrator
 	var permissions = msg.member.permissions;
 	if (permissions.hasPermission('ADMINISTRATOR') || permissions.hasPermission('MANAGE_CHANNELS')) {
 		return true;
 	}
+	
+	//Set the required level of permission
 	if (role === roleMember) {
 		permLevel = 1;
 	} else if (role === roleModo) {
 		permLevel = 2;
 	}
+	
+	//Set the user permission level
 	for (i = 0; i < msg.member.roles.array().length; i++) {
 		if (msg.member.roles.array()[i].name === config.roleModo) {
 			currentPermLevel = 2;
@@ -245,6 +302,8 @@ function checkRole(msg, role) {
 			currentPermLevel = 1;
 		}
 	}
+	
+	//Compare user and needed permission level
 	if (currentPermLevel < permLevel) {
 		console.log("Not enough permissions");
 		return false;
@@ -253,6 +312,7 @@ function checkRole(msg, role) {
 	}
 }
 
+//Make sure the process exits correctly and don't fails to close
 process.on('SIGINT', function () {
 	process.exit(2);
 });
