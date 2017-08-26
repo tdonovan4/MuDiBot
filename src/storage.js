f560e7eb0d8365db6e9e3d1b5aa249ff9269e16e
tdonovan4 <tdonovan@codengineer.com>
2017-08-26 00:38:16 -0400

    Add modifytext to storage.js

--- src/storage.js
+++ src/storage.js
@@ -4,16 +4,29 @@ const fs = require('fs');
 module.exports = {
 	write: function (file, obj) {
 		var json = JSON.stringify(obj);
-		
+
 		fs.writeFile(file, json, 'utf8', (err) => {
 			if (err){ console.log(err); }
 		});
 	},
-	
+
 	read: function (file) {
 		return JSON.parse(fs.readFileSync(file, 'utf8'));
 	},
-	
+
+	modifyText: function (file, text, value) {
+		fs.readFile(file, 'utf8', function (err,data) {
+		  if (err) {
+		    return console.log(err);
+		  }
+		  var result = data.replace(text, value);
+
+		  fs.writeFile(file, result, 'utf8', function (err) {
+		     if (err) return console.log(err);
+		  });
+		});
+	},
+
 	empty: function (file) {
 		if(fs.readFileSync(file, 'utf8') == '') {
 			return true;
@@ -21,14 +34,14 @@ module.exports = {
 			return false;
 		}
 	},
-	
+
 	exist: function (file) {
 		if (!fs.existsSync(file)) {
 			fs.openSync(file, 'w');
-		}	
+		}
 	},
-	
+
 	delete: function (file) {
 		fs.unlinkSync(file);
 	}
-}	
+}
