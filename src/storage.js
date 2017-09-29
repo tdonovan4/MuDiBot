a6b351d49ef0cb1731c340fa3149f0aef244e7fc
tdonovan4 <tdonovan@codengineer.com>
2017-09-28 23:06:46 -0400

    Change storage files
    * Change storage files to include more information
    * Change warnings to work with the new storage files
    * Change some little things in warnings

--- src/storage.js
+++ src/storage.js
@@ -3,9 +3,9 @@ const fs = require('fs');
 //TODO: Add argument for path
 module.exports = {
 	write: function (file, obj) {
-		var json = JSON.stringify(obj);
+		var json = JSON.stringify(obj, null, '\t');
 
-		fs.writeFile(file, json, 'utf8', (err) => {
+		fs.writeFileSync(file, json, 'utf8', (err) => {
 			if (err){ console.log(err); }
 		});
 	},
@@ -27,17 +27,46 @@ module.exports = {
 		});
 	},
 
-	empty: function (file) {
-		if(fs.readFileSync(file, 'utf8') == '') {
-			return true;
-			} else {
-			return false;
+	checkStorageFile: function (file, msg) {
+		var example = this.read('./storage/storage-file-example.json').fileExample;
+		//Check if the file as been created
+		if (fs.existsSync(file)) {
+			var storageFile = this.read(file);
+			//Example file
+			//TODO: Make this a function
+			let exampleKeys = Object.keys(example);
+
+			for(i = 0; i < exampleKeys.length; i++) {
+				if(!(exampleKeys[i] in storageFile)) {
+						storageFile[exampleKeys[i]] = example[exampleKeys[i]];
+				}
+			}
+			storageFile.server = msg.guild.name;
+			this.write(file, storageFile);
+		} else {
+			example.server = msg.guild.name;
+			this.write(file, example);
 		}
 	},
 
-	exist: function (file) {
-		if (!fs.existsSync(file)) {
-			fs.openSync(file, 'w');
+	checkUser: function (file, msg, user) {
+		let example = this.read('./storage/storage-file-example.json').userExample;
+		let storageFile = this.read(file);
+		//Check if the file as been created
+		if(user.id in storageFile.users) {
+			//Example file
+			let exampleKeys = Object.keys(example);
+			for(i = 0; i < exampleKeys.length; i++) {
+				if(!(exampleKeys[i] in storageFile.users[user.id])) {
+					storageFile.users[user.id][exampleKeys[i]] = example[exampleKeys[i]];
+				}
+			}
+			storageFile.users[user.id].name = user.username;
+			this.write(file, storageFile);
+		} else {
+			storageFile.users[user.id] = example;
+			storageFile.users[user.id].name = user.username;
+			this.write(file, storageFile);
 		}
 	},
 
