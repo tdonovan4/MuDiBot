1b53f9c6cb9b4f4936d3608b222457550afa107b
tdonovan4 <tdonovan@codengineer.com>
2017-10-14 23:19:32 -0400

    Organize config.js

--- config.js
+++ config.js
@@ -1,24 +1,39 @@
 //Configuration file
 //Please fill the fields
 module.exports = {
+	/*===============
+		-Credentials-
+	===============*/
 	//The token of the bot
 	botToken: '',
 	//Your YouTube Data API key
 	youtubeAPIKey: '',
+
+	/*===============
+		-General-
+	===============*/
 	//The prefix for a command (for example $help)
 	prefix: '$',
+	//Localization ('english' or 'french')
+	language: '',
+	//The 'game' the bot is playing (more like a status)
+	status: 'Type $help',
+	//List of users (id) with all permissions
+	superusers: [''],
+
+	/*===============
+		-Roles-
+	===============*/
 	//Role name for "botMember"
 	roleMember: '',
 	//Role name for "botModo"
 	roleModo: '',
-	//The 'game' the bot is playing (more like a status)
-	status: 'Type $help',
-	//Localization ('english' or 'french')
-	language: '',
+
+	/*===============
+		-Clear list-
+	===============*/
 	//List of commands to clear with "$clearlog"
 	commandsToClear: [''],
 	//List of users (id) to clear with "$clearlog"
 	usersToClear: [''],
-	//List of users (id) with all permissions
-	superusers: [''],
 }
