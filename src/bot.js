1da80a839e0170664ad171b7d29dc6683e227ad8
tdonovan4 <tdonovan@codengineer.com>
2017-12-16 23:17:52 -0500

    Little fixes for profile

--- src/bot.js
+++ src/bot.js
@@ -182,19 +182,19 @@ var commands = {
       let level = progression[0];
       let xpToNextLevel = `${progression[1]}/${levels.getXpForLevel(level)}`;
       let rank = levels.getRank(progression[2]);
-      let groups = [''];
+      let groups = ['Ø'];
 
       //Get groups
       if(userData.groups != null) {
-        userData.groups.split(',').sort(function(a, b) {
+        groups = userData.groups.split(',').sort(function(a, b) {
           return config.groups.find(x => x.name == a).permLvl <
             config.groups.find(x => x.name == b).permLvl;
         });
       }
 
       //If user is a superuser, add that to groups
-      if(config.superusers.find(x => x == msg.author.id) != null) {
-        groups.unshift('Superuser')
+      if(config.superusers.find(x => x == user.id) != null) {
+        groups.unshift('Superuser');
       }
 
       //Put newline at every 4 groups
