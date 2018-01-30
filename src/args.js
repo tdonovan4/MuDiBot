474d0eb4b46ee64a21797ffcf6f0bc1e39c8eb32
tdonovan4 <tdonovan@codengineer.com>
2018-01-29 22:21:25 -0500

    Use environment variable for custom config
    Will be useful with mocha

--- src/args.js
+++ src/args.js
@@ -1,17 +1,7 @@
-function checkFor(args, target) {
-  for (var i = 0; i < args.length; i++) {
-    if (args[i].substring(0, 8) == target) {
-      return args[i + 1];
-      console.log(target);
-    }
-  }
-}
-
 module.exports = {
   getConfig: function() {
-    var args = process.argv.slice(2);
-    //Check if an argument starts with --config
-    var customConfig = checkFor(args, '--config');
+    //Check if there is an environment variable for custom config
+    var customConfig = process.env.CONFIG
 
     var config = customConfig == undefined ? '../config.js': customConfig;
     return require(config);
