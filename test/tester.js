6e801f727762976533a8622d74138c5a08647f9e
tdonovan4 <tdonovan@codengineer.com>
2018-02-08 22:12:47 -0500

    Implement first test cases
    Test checkIfValidCmd()

--- test/tester.js
+++ test/tester.js
@@ -1,13 +1,29 @@
-var expect = require('chai').expect;
+const expect = require('chai').expect;
+const sinon = require('sinon');
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const bot = require('../src/bot.js');
+const commands = require('../src/commands.js');
 var config = require('../src/args.js').getConfig();
+var msg = require('./test-messages/test-message1.json');
 
-describe('Startup tests', function() {
-  describe('Token', function() {
-    it('Should login if token is present', async() => {
-      const login = await client.login(config.botToken)
-      expect(login).to.equal(config.botToken)
+describe('Test commands', function() {
+  describe('Execute command', function() {
+    it('Should return false when using a false command', async function() {
+      //Change content of message
+      msg.content = 'randomString';
+      var response = await commands.checkIfValidCmd(msg, ['randomString']);
+      expect(response).to.equal(false);
     });
+    before(function() {
+      var stub = sinon.stub(commands, 'checkPerm');
+      stub.resolves(true);
+    });
+    it('Should return true when using a real command', async function() {
+      msg.content = '$help';
+      var response = await commands.checkIfValidCmd(msg, ['help']);
+      console.log(response);
+      expect(response).to.equal(true);
+    })
   });
 });
