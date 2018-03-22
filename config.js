ce30aa2206f419b27ebdc5ed7f1eb4fcdfaeb092
tdonovan4 <tdonovan@codengineer.com>
2018-03-21 23:32:54 -0400

    Fix typo in config

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ module.exports = {
   //The 'game' the bot is playing (more like a status)
   currentStatus: 'Type $help',
   //The path to the database (default: './storage/data.db')
-  databasePath: './storage/data.db',
+  pathDatabase: './storage/data.db',
   //List of users (id) with all permissions
   superusers: [''],
 
