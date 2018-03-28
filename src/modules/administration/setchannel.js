aa241939ca382b177ae5decfa3107c61af84f879
tdonovan4 <tdonovan@codengineer.com>
2018-03-27 22:37:29 -0400

    Convert setchannel to a class

---
+++ src/modules/administration/setchannel.js
@@ -0,0 +1,20 @@
+const bot = require('../../bot.js');
+const defaultChannel = require('../../default-channel.js');
+var lang = require('../../localization.js').getLocalization();
+
+module.exports = class SetChannelCommand extends bot.Command {
+  constructor() {
+    super({
+      name: 'setchannel',
+      aliases: [],
+      category: 'administration',
+      permLvl: 3
+    });
+  }
+  execute(msg, args) {
+    var botChannel = msg.channel;
+    //Modify default channel in database
+    defaultChannel.setChannel(msg, botChannel);
+    botChannel.send(lang.setchannel.newDefaultChannel);
+  }
+}
