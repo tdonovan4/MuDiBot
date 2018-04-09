80a8e9cbe42b0f9e0d4f0aaf593fe04616477460
tdonovan4 <tdonovan@codengineer.com>
2018-04-08 21:29:36 -0400

    Rework roll

--- src/modules/fun/random.js
+++ src/modules/fun/random.js
@@ -28,12 +28,46 @@ module.exports = {
     }
     execute(msg, args) {
       args = msg.content.split(/[ d+]|(?=-)/g).slice(1);
-      var num = isNaN(args[2]) ? 0 : parseInt(args[2]);
-      console.log(num)
-      for (i = 0; i < args[0]; i++) {
-        num += Math.floor(Math.random() * args[1]) + 1;
+      var invalid = false;
+      function checkIfValid(string) {
+        //Return false if whitespace because apparently " " is a number in JavaScript
+        if(string == /\s/g.test(string)) {
+          return false;
+        }
+        result = isNaN(string) || string > 50;
+        return !result;
       }
-      msg.reply(num);
+      //Get values
+      var numDice = 1;
+      if (checkIfValid(args[0]) || args[0] > 1){
+        numDice = parseInt(args[0]);
+      } else {
+        invalid = true;
+      }
+      var die = 6;
+      if (checkIfValid(args[1])) {
+        die = parseInt(args[1]);
+      } else {
+        invalid = true;
+      }
+      var bonus = checkIfValid(args[2]) ? parseInt(args[2]) : 0;
+      //RNG
+      var dice = [];
+      for (var i = 0; i < numDice; i++) {
+        dice.push(Math.floor(Math.random() * die) + 1);
+      }
+      //Make message
+      var reply = '';
+      if (invalid) {
+        //print warning message
+        reply += lang.roll.invalid;
+      }
+      reply += `(${dice.join(' + ')})`;
+      if (bonus > 0) {
+        reply += ` + ${bonus}`;
+      }
+      reply += ` = ${dice.reduce((a, b) => a+b) + bonus}`;
+      msg.channel.send(reply);
     }
   }
 }
