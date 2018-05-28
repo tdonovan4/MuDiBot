7f842dd72684ac7d8e1c5af792c24ad9bafafd3c
tdonovan4 <tdonovan@codengineer.com>
2018-05-27 21:22:49 -0400

    Convert giphy-api.js
    Convert giphy-api.js to async/await.

--- src/modules/fun/giphy-api.js
+++ src/modules/fun/giphy-api.js
@@ -55,29 +55,24 @@ function get(url) {
   });
 }
 
-function search(args) {
+async function search(args) {
   //Search on giphy for a GIF and output URL
-  return new Promise((resolve) => {
-    var url;
-    if (args[0] != undefined) {
-      //Search
-      url = `https://api.giphy.com/v1/gifs/search?api_key=${config.giphyAPIKey}&q=${args[0]}&limit=1`;
-    } else {
-      //Get a trending GIF
-      url = `https://api.giphy.com/v1/gifs/trending?api_key=${config.giphyAPIKey}&limit=1`;
-    }
-    get(url).then(function(response) {
-      //Return the url
-      resolve(response.data[0].url);
-    });
-  });
+  var url;
+  if (args[0] != undefined) {
+    //Search
+    url = `https://api.giphy.com/v1/gifs/search?api_key=${config.giphyAPIKey}&q=${args[0]}&limit=1`;
+  } else {
+    //Get a trending GIF
+    url = `https://api.giphy.com/v1/gifs/trending?api_key=${config.giphyAPIKey}&limit=1`;
+  }
+  var response = await get(url);
+  //Return the GIF url
+  return response.data[0].url;
 }
 
-function random(args) {
-  return new Promise((resolve) => {
-    get(`https://api.giphy.com/v1/gifs/random?api_key=${config.giphyAPIKey}${args[0] == undefined ? '' : '&tag=' + args[0]}&limit=1&rating=g`)
-      .then(function(response) {
-        resolve(response.data.url)
-      });
-  });
+async function random(args) {
+  var response = await get('https://api.giphy.com/v1/gifs/random?api_key=' +
+    `${config.giphyAPIKey}${args[0] == undefined ? '' : '&tag=' + args[0]}&limit=1&rating=g`);
+  //Return the random GIF url
+  return response.data.url;
 }
