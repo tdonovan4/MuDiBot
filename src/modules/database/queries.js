0e4c00f89286c1d12081fc08c577950e402df4aa
Thomas Donovan <tdonovan@codengineer.com>
2018-06-17 19:25:09 -0400

    Add checker.js
    For the moment, only add missing tables. #102

--- src/modules/database/queries.js
+++ src/modules/database/queries.js
@@ -1,15 +1,10 @@
 const sql = require('sqlite');
 const config = require('../../args.js').getConfig()[1];
-const checkTable = 'CREATE TABLE IF NOT EXISTS users (serverId TEXT, userId TEXT,' +
-  `xp INTEGER DEFAULT 0, warnings INTEGER DEFAULT 0, groups TEXT DEFAULT "${config.groups[0].name}",` +
-  'CONSTRAINT users_unique UNIQUE (serverId, userId))';
 
 module.exports = {
   runGetQuery: async function(query, args) {
     try {
       await sql.open(config.pathDatabase);
-      //Make sure table exists
-      await sql.run(checkTable);
       var response = await sql.get(query, args);
       await sql.close();
     } catch (e) {
@@ -21,8 +16,6 @@ module.exports = {
   runAllQuery: async function(query, args) {
     try {
       await sql.open(config.pathDatabase);
-      //Make sure table exists
-      await sql.run(checkTable);
       var response = await sql.all(query, args);
       await sql.close();
     } catch (e) {
@@ -34,8 +27,6 @@ module.exports = {
   runUpdateQueryUser: async function(query, serverId, userId, newValue) {
     try {
       await sql.open(config.pathDatabase);
-      //Make sure table exists
-      await sql.run(checkTable);
       //If user don't exist, insert
       await sql.run(`INSERT OR IGNORE INTO users (serverId, userId) VALUES (?, ?)`, [
         serverId, userId
@@ -50,8 +41,6 @@ module.exports = {
   runUpdateQueryUsers: async function(query, userId, newValue) {
     try {
       await sql.open(config.pathDatabase);
-      //Make sure table exists
-      await sql.run(checkTable);
       //Update user
       await sql.run(query, [newValue, userId]);
       await sql.close();
