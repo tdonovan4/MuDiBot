009aa27fb14b42deff9542ccd9816052e7156a80
Thomas Donovan <tdonovan@codengineer.com>
2018-07-08 00:22:08 +0200

    Add modifyprofile input conditions

--- src/modules/user/profile.js
+++ src/modules/user/profile.js
@@ -6,6 +6,84 @@ const mustache = require('mustache');
 var config = require('../../args.js').getConfig()[1];
 var lang = require('../../localization.js').getLocalization();
 
+class Condition {
+  constructor(name, value) {
+    this.name = name;
+    this.value = value;
+  }
+}
+
+class MaxCharLengthCondition extends Condition {
+  constructor(maxCharLength) {
+    super('maxCharLength', maxCharLength);
+  }
+  validate(input) {
+    /*
+     * If a maximum character limit is set,
+     * check the number of characters of
+     * the input and return false if too big
+     */
+    return this.value == undefined ||
+      input.length <= this.value;
+  }
+}
+
+class FormatCondition extends Condition {
+  constructor(regex) {
+    super('format', regex);
+  }
+  validate(input) {
+    var result = true;
+    if(this.value != undefined) {
+      //Test the input against the provided regex
+      result = this.value.test(input);
+    }
+    return result
+  }
+}
+
+class ProfileField {
+  constructor(name, req) {
+    /*
+     * The name of the field, used in
+     * the command to find which column
+     * in the database too change.
+     */
+    this.name = name;
+    //Conditions used to validate the input
+    this.conditions = [
+      //The maximum amount of characters for the input
+      new MaxCharLengthCondition(req.maxCharLength),
+      //The format of the input using regex
+      new FormatCondition(req.format)
+    ]
+  }
+  isInputValid(input) {
+    var isValid = true;
+    //Check each conditions to make sure the input is valid
+    for(var condition of this.conditions) {
+      var result = condition.validate(input);
+      if (result === false) {
+        //If one condition failed, the input is not valid
+        isValid = false;
+        break;
+      }
+    }
+    return isValid;
+  }
+}
+
+//The user profile customizable fields
+var profileFields = new Map();
+//Set the fields
+profileFields.set('bio', new ProfileField('bio', {
+  maxCharLength: 280
+}));
+profileFields.set('birthday', new ProfileField('birthday', {
+  format: /([0-9]{4}|-)(-[0-9]{2}){2}/
+}));
+profileFields.set('location', new ProfileField('location', {}));
+
 module.exports = {
   ProfileCommand: class extends bot.Command {
     constructor() {
@@ -75,8 +153,23 @@ module.exports = {
         permLvl: 0
       });
     }
-    async execute(msg, args) {
-      //TODO: Add the command
+    execute(msg, args) {
+      //Check args
+      if(args.length < 2) {
+        if(args.length < 1) {
+          //No field
+          bot.printMsg(msg, lang.error.missingArg.field);
+        } else {
+          //No value
+          bot.printMsg(msg, lang.error.missingArg.value);
+        }
+        return;
+      }
+      //Check input
+      var isInputValid = profileFields.get(args[0]).isInputValid(args[1]);
+      if(isInputValid) {
+        //TODO: Add to database
+      }
     }
   }
 }
